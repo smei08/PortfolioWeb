3ffcfa9bec0b6884c2f36b0ecfe1e600dff9b1c8
Stephanie Mei <smei826@gmail.com>
2025-09-30 17:23:06 -0400

    browser router added in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Portfolio</h1>
+      <p>Welcome to my site!</p>
     </>
   )
 }
 
-export default App
+export default App;
