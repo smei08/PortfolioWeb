10a6c48a5fc987b14bbfec033b7e07a24b7ad407
Stephanie Mei <smei826@gmail.com>
2026-07-14 15:06:20 -0400

    added header border

--- src/sections/CalendarHeader/CalendarHeader.jsx
+++ src/sections/CalendarHeader/CalendarHeader.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import CalendarDate from "../../components/Date/CalendarDate";
 import "./calendarHeader.css";
+import knotBorder from "../../assets/knot-border.svg";
 
 export default function CalenderHeader() {
   return (
     <div className="calender-header-container">
-      <span>
-        丙午年
-        <br />
-        二〇二六
-        <br />
-        Year of the Horse
-      </span>
-      <CalendarDate show="year" />
-      <span>
-        Avaiable for Work
-        <br />
-        Front-End Dev
-      </span>
+      <img src={knotBorder} alt="" className="knot-border" />
+      <div className="header-content">
+        <span>
+          丙午年
+          <br />
+          二〇二六
+          <br />
+          Year of the Horse
+        </span>
+        <CalendarDate show="year" />
+        <span>
+          Available for Work
+          <br />
+          Front-End Dev
+        </span>
+      </div>
     </div>
   );
 }
